4c190c3cd32e1dd248962f4a2f6eb3e64e0ab466
Gabriela Melo <gaabmmelo@gmail.com>
2024-01-30 14:37:12 -0300

    Backend - rota api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', 'UserController@store');
+Route::post('/register', [UserController::class, 'store'])->name('users.store');
